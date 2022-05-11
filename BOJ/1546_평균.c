8d49d7d291f033ba3a921e00ba7639808313c77d
ownvoy <wonjun703@naver.com>
2022-05-11 12:49:58 +0900

    배열 1부터 시작한거 0으로 고쳐 주고, 크기 조절해줌

--- BOJ/1546_평균.c
+++ BOJ/1546_평균.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 int max_finder(int* array,int Size){
-    int max = array[1];
-    for (int i = 1;i <= Size-1;i++){
+    int max = array[0];
+    for (int i = 0;i <= Size-2;i++){
         if (array[i]<array[i+1]){
             max = array[i+1];
 
@@ -12,34 +12,40 @@ int max_finder(int* array,int Size){
 
 }
 
-double scam_mean(int* array, int Size,int max){
+double scam_mean(int *array, int Size, int max)
+{
 
     double scam_score[Size];
-    double scam_sum=0;
-    
-    for (int i=1; i<=Size; i++){
-        scam_score[i] = array[i]/max*100;
-        //printf("%f\n", scam_score[i]);
+    double array_double[Size];
+    double scam_sum = 0;
+    double max_double = max;
+
+    for (int i = 0; i <= Size - 1; i++)
+    {
+        array_double[i] = array[i];
+        //printf("%d 'th score: %f\n", i + 1, array_double[i]);
+        scam_score[i] = array_double[i] / max_double * 100;
+
+        //printf("scam_score[%d]= %f\n", i, scam_score[i]);
         scam_sum += scam_score[i];
     }
 
-    double mean= scam_sum/ Size;
+    double mean = scam_sum / Size;
     return mean;
 }
-
 
 int main(){
     int size;
     scanf("%d",&size);
     int subject_score[size];
-
     
-    for (int i = 1;i <= size;i++){
+    for (int i = 0;i <= size-1;i++){
         scanf("%d", &subject_score[i]);
     }
 
     int max = max_finder(subject_score, size);
-    //printf("%d\n",max);
+    //printf("max: %d\n",max);
+    //printf("subject score: %d\n", subject_score[1]);
     double scam = scam_mean(subject_score, size, max);
 
     printf("%f",scam);
