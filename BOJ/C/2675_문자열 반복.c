5ba195d2e0756d675081dc3aecb3c0cdedfcfb90
ownvoy <wonjun703@naver.com>
2022-06-10 13:05:48 +0900

    구조체 + malloc

---
+++ BOJ/C/2675_문자열 반복.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct _test{
+    int repeat;
+    char string[];
+} test;
+
+int main(){
+    int testcase;
+    scanf("%d",&testcase);
+
+    for (int i=0; i<testcase; i++)
+    {
+        test ap;
+
+        scanf("%d", &ap.repeat);
+        getchar();
+        scanf("%s", ap.string);
+        int stringlen = strlen(ap.string);
+        if (stringlen > 0){
+            char *newstrptr = (char *)malloc(sizeof(char)*(stringlen+1));
+            strcpy(newstrptr, ap.string);
+
+            for (int j = 0; j < sizeof(newstrptr) / sizeof(char); j++)
+            {
+                for (int h = 0; h < ap.repeat; h++)
+                {
+                    printf("%c", newstrptr[j]);
+                }
+            }
+            free(newstrptr);
+        }
+        printf("\n");
+    }
+
+    
+    return 0;
+    
+}
